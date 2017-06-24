454ae8d311413feeda9e4af09388d0487515c9f1
JuneCho23 <junecho.ucb@gmail.com>
2017-06-24 15:43:55 -0700

    ImageCell

--- ASampleApp/ASampleApp/View/ListOfDogsPicturesPage.cs
+++ ASampleApp/ASampleApp/View/ListOfDogsPicturesPage.cs
@@ -1,10 +1,43 @@
 ﻿using System;
+using Xamarin.Forms;
+using ASampleApp.Data;
+using ASampleApp.Helper;
+using ASampleApp.View.Base;
+using ASampleApp.ViewModel;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
 namespace ASampleApp.View
 {
-    public class ListOfDogsPicturesPage
+    public class ListOfDogsPicturesPage : BaseContentPage<ListOfDogsViewModel>
     {
+        ListView _dogList;
+
         public ListOfDogsPicturesPage()
         {
+            _dogList = new ListView();
+            _dogList.SetBinding(ListView.ItemsSourceProperty, nameof(MyViewModel.ListOfDoggos));
+
+            var myTemplate = new DataTemplate(typeof(ImageCell));
+            var model = BindingContext as Dog;
+            myTemplate.SetBinding(ImageCell.TextProperty, nameof(model.Name));
+            myTemplate.SetBinding(ImageCell.DetailProperty, nameof(model.FurColor));
+
+            _dogList.ItemTemplate = myTemplate;
+
+            Content = new StackLayout
+            {
+                Margin = 20,
+                Children = {
+                    _dogList
+                }
+            };
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            //Hack1-simply set properties each time page appears
+            //MyViewModel.ListOfDoggos = App.DogRepo.GetAllDogs();
         }
     }
 }
