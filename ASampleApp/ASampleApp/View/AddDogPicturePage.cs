6806964413884c2426f215f61406af7ffa7dab17
JuneCho23 <junecho.ucb@gmail.com>
2017-06-24 17:46:39 -0700

    nuke database, created new one

--- ASampleApp/ASampleApp/View/AddDogPicturePage.cs
+++ ASampleApp/ASampleApp/View/AddDogPicturePage.cs
@@ -10,27 +10,53 @@ namespace ASampleApp.View
 {
     public class AddDogPicturePage : BaseContentPage<FirstViewModel>
     {
-		Entry _dogNameEntry = new Entry { Placeholder = "Enter Name of Dog" };
+        Entry _dogNameEntry = new Entry { Placeholder = "Enter Name of Dog" };
         Entry _dogFurColorEntry = new Entry { Placeholder = "Enter Dog Fur Color" };
-		Entry _dogPictureURL = new Entry { Text = "https://kasehavanese.files.wordpress.com/2014/10/izzyy1.jpg?w=519&h=346&crop=1" };
-		Button _submitNameandFurColorButton = new Button { Text = "Submit" };
+        Entry _dogPictureURL = new Entry { Text = "https://s-media-cache-ak0.pinimg.com/736x/a9/e5/49/a9e5491335b070025a517cf748bb317c--havanese-puppies-teacup-puppies.jpg" };
+        Button _submitNameandFurColorButton = new Button { Text = "Submit" };
 
 
         public AddDogPicturePage()
         {
+            _dogFurColorEntry.SetBinding(Entry.TextProperty, "FurColorOfDog");
+            _dogNameEntry.SetBinding(Entry.TextProperty, "DogName");
+            _submitNameandFurColorButton.SetBinding(Button.CommandProperty, "DisplayItemCommand");
 
-			Content = new StackLayout
-			{
-				Margin = 20,
-				Children = {
-					_dogNameEntry,
-					_dogFurColorEntry,
+
+            Content = new StackLayout
+            {
+                Margin = 20,
+                Children = {
+                    _dogNameEntry,
+                    _dogFurColorEntry,
                     _dogPictureURL,
-					_submitNameandFurColorButton,
-					
-				}
-			};
+                    _submitNameandFurColorButton,
+
+                }
+            };
 
         }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _submitNameandFurColorButton.Clicked += _submitNameandFurColorButton_Clicked;
+
+
+        }
+
+        void _submitNameandFurColorButton_Clicked(object sender, EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(()=>Navigation.PushAsync(new ListOfDogsPicturesPage()));
+        }
+
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            _submitNameandFurColorButton.Clicked -= _submitNameandFurColorButton_Clicked;
+
+        }
+
     }
 }
