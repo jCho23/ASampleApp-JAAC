3e6617389e369d35562264cc80f76dd6c7d927fd
JuneCho23 <junecho.ucb@gmail.com>
2017-06-26 13:42:27 -0700

    Cleaned up Text to Speech Page

--- ASampleApp/ASampleApp/View/TextToSpeechPage.cs
+++ ASampleApp/ASampleApp/View/TextToSpeechPage.cs
@@ -1,10 +1,101 @@
 ﻿using System;
+using Xamarin.Forms;
+using ASampleApp.View.Base;
+using ASampleApp.ViewModel;
+using ASampleApp.Data;
+
 namespace ASampleApp.View
 {
-    public class TextToSpeechPage
-    {
-        public TextToSpeechPage()
-        {
-        }
-    }
+	public class TextToSpeechPage : BaseContentPage<TextToSpeechViewModel>
+	{
+		public TextToSpeechPage()
+		{
+			//this.SetBinding(IsBusyProperty, nameof(ViewModel.IsInternetConnectionInUse));
+			var takePictureButton = new Button { Text = "   Take A Picture Of Text   " };
+			//var inverseBoolConverter = new InverseBoolConverter();
+			//var inverseBoolBinding = new Binding(nameof(ViewModel.IsActivityIndicatorDisplayed), BindingMode.Default, inverseBoolConverter, ViewModel.IsActivityIndicatorDisplayed);
+			//takePictureButton.SetBinding(IsVisibleProperty, inverseBoolBinding);
+            takePictureButton.SetBinding(Button.CommandProperty, nameof(MyViewModel.TakePictureButtonCommand));
+			var spokenTextLabel = new Label { HorizontalTextAlignment = TextAlignment.Center };
+			spokenTextLabel.SetBinding(Label.TextProperty, nameof(MyViewModel.SpokenTextLabelText));
+			//var activityIndicatorLabel = new Label { FontAttributes = FontAttributes.Italic };
+			//activityIndicatorLabel.SetBinding(Label.TextProperty, nameof(ViewModel.ActivityIndicatorLabelText));
+			//var activityIndicator = new ActivityIndicator();
+			//activityIndicator.SetBinding(IsVisibleProperty, nameof(ViewModel.IsActivityIndicatorDisplayed));
+			//activityIndicator.SetBinding(ActivityIndicator.IsRunningProperty, nameof(ViewModel.IsActivityIndicatorDisplayed));
+			var stackLayout = new StackLayout
+			{
+				Margin = new Thickness(20, 0),
+				VerticalOptions = LayoutOptions.Center,
+				HorizontalOptions = LayoutOptions.Center,
+				Children = {
+					spokenTextLabel,
+					//activityIndicatorLabel,
+					//activityIndicator,
+					takePictureButton,
+				}
+			};
+			Content = new ScrollView { Content = stackLayout };
+			Title = "XamSpeak";
+		}
+		#region Methods
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			//ViewModel.OCRFailed += HandleOCRFailed;
+			//ViewModel.SpellCheckFailed += HandleSpellCheckFailed;
+			MyViewModel.NoCameraDetected += HandleNoCameraDetected;
+			//ViewModel.InvalidComputerVisionAPIKey += HandleInvalidComputerVisionAPIKey;
+			//HttpHelpers.InvalidBingSpellCheckAPIKey += HandleInvalidBingSpellCheckAPIKey;
+			//ViewModel.InternetConnectionUnavailable += HandleInternetConnectionUnavailable;
+			//HttpHelpers.Error429_TooManySpellCheckAPIRequests += HandleError429_TooManySpellCheckAPIRequests;
+		}
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+			//ViewModel.OCRFailed -= HandleOCRFailed;
+			//ViewModel.SpellCheckFailed -= HandleSpellCheckFailed;
+			MyViewModel.NoCameraDetected -= HandleNoCameraDetected;
+			//ViewModel.InvalidComputerVisionAPIKey -= HandleInvalidComputerVisionAPIKey;
+			//HttpHelpers.InvalidBingSpellCheckAPIKey -= HandleInvalidBingSpellCheckAPIKey;
+			//ViewModel.InternetConnectionUnavailable -= HandleInternetConnectionUnavailable;
+			//HttpHelpers.Error429_TooManySpellCheckAPIRequests -= HandleError429_TooManySpellCheckAPIRequests;
+		}
+		//void HandleInternetConnectionUnavailable(object sender, EventArgs e)
+		//{
+		//	Device.BeginInvokeOnMainThread(async () =>
+		//									   await DisplayAlert("Error", "Internet Connection Unavailable", "Ok"));
+		//}
+		//void HandleInvalidComputerVisionAPIKey(object sender, EventArgs e)
+		//{
+		//	Device.BeginInvokeOnMainThread(async () =>
+		//									await DisplayAlert("Error", "Invalid Computer Vision API Key", "Ok"));
+		//}
+		//void HandleInvalidBingSpellCheckAPIKey(object sender, EventArgs e)
+		//{
+		//	Device.BeginInvokeOnMainThread(async () =>
+		//									await DisplayAlert("Error", "Invalid Bing Spell Check API Key", "Ok"));
+		//}
+		//void HandleSpellCheckFailed(object sender, EventArgs e)
+		//{
+		//	Device.BeginInvokeOnMainThread(async () =>
+		//									await DisplayAlert("Error", "Spell Check Failed", "Ok"));
+		//}
+		void HandleNoCameraDetected(object sender, EventArgs e)
+		{
+			Device.BeginInvokeOnMainThread(async () =>
+										   await DisplayAlert("Error", "No Camera Available", "Ok"));
+		}
+		//void HandleError429_TooManySpellCheckAPIRequests(object sender, EventArgs e)
+		//{
+		//	Device.BeginInvokeOnMainThread(async () =>
+		//									await DisplayAlert("Error", "Bing Spell Check API Limit Reached", "Ok"));
+		//}
+		//void HandleOCRFailed(object sender, EventArgs e)
+		//{
+		//	Device.BeginInvokeOnMainThread(async () =>
+		//									await DisplayAlert("Error", "Optical Character Recognition Failed", "Ok"));
+		//}
+		#endregion
+	}
 }
